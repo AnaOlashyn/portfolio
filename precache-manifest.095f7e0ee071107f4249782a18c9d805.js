4729f7933f31645972c809fb67c73c5adbd58cd0
AnaOlashyn <anaolashyn@gmail.com>
2019-04-22 17:47:57 +0300

    Updates

---
+++ precache-manifest.095f7e0ee071107f4249782a18c9d805.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "20fa8deea6a1819e41019da9eb291500",
+    "url": "/portfolio/static/media/flowers-1230225s.20fa8dee.jpg"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "789e686fdd512adccf2b",
+    "url": "/portfolio/static/js/main.444e5be9.chunk.js"
+  },
+  {
+    "revision": "3aa3ec3b8013c8b3daef",
+    "url": "/portfolio/static/js/2.04621e33.chunk.js"
+  },
+  {
+    "revision": "789e686fdd512adccf2b",
+    "url": "/portfolio/static/css/main.9a5a6690.chunk.css"
+  },
+  {
+    "revision": "876b37fcae95123a700ec172c3d92866",
+    "url": "/portfolio/index.html"
+  }
+];
